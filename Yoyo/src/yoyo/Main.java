a806453cad9fff98bb87bedcb0b10e4b880fd5e2
DuranDurannn <darrenkoh45@gmail.com>
2024-01-20 23:21:02 +0800

    Finally, it is something WWAWWWWW

---
+++ Yoyo/src/yoyo/Main.java
@@ -0,0 +1,23 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package yoyo;
+
+/**
+ *
+ * @author Sammy Guergachi <sguergachi at gmail.com>
+ */
+public class Main {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        Login loginFrame = new Login();
+        loginFrame.setVisible(true);
+        loginFrame.pack();
+        loginFrame.setLocationRelativeTo(null);
+    }
+    
+}
