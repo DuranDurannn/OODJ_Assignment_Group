e5833e58574eb174677bada17f0ef743b1b34748
Xenon0307 <wa9398d@gmail.com>
2024-02-15 11:29:51 +0800

    All UI except customer stuff

--- Yoyo/src/yoyo/application/Main.java
+++ Yoyo/src/yoyo/application/Main.java
@@ -1,45 +1,21 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
-package yoyo.application;
-import java.io.BufferedReader;
-import java.io.FileReader;
-import java.io.IOException;
-import yoyo.login.LoginUI;
 
-/**
- *
- * @author Eow
- */
-public class Main {
-     public static void main(String args[]) {
-        LoginUI LoginFrame = new LoginUI();
-        LoginFrame.setVisible(true);
-        LoginFrame.pack();
-        LoginFrame.setLocationRelativeTo(null);
+import com.formdev.flatlaf.FlatIntelliJLaf;
+import javax.swing.UIManager;
+import yoyo.application.DashboardUI;
+import yoyo.reports.ClosedSaleReport;
+import yoyo.reports.WorkDoneReport;
 
-        // Call a method to open and read the user registration file
-        readUserRegistrationFile();
-    }
 
-    private static void readUserRegistrationFile() {
-        // Specify the file path
-        String filePath = "userRegistration.txt";
+public static void main(String args[]) {
         
-
-        // Open and read the file
-        try (BufferedReader reader = new BufferedReader(new FileReader(filePath))) {
-            String line;
-            while ((line = reader.readLine()) != null) {
-                // Display each line (user entry) in the console
-                System.out.println(line);
-            }
-        } catch (IOException e) {
-            // Handle the exception (e.g., show an error message)
-            e.printStackTrace();
-        }
+    try {
+        UIManager.setLookAndFeel (new FlatIntelliJLaf()); 
+    } catch (Exception e) {
+        e.printStackTrace();
     }
+
+    new DashboardUI().setVisible(true);
+        
 }
 
         
