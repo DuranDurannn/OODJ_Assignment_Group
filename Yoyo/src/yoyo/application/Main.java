f64d8c8c2c7a1aa4b7749934c0ee635038b353d9
SmurfEow <tiramisu026@gmail.com>
2024-02-15 17:39:52 +0800

    Trying to fix the error

--- Yoyo/src/yoyo/application/Main.java
+++ Yoyo/src/yoyo/application/Main.java
@@ -4,19 +4,19 @@ import javax.swing.UIManager;
 import yoyo.application.DashboardUI;
 
 
-public static void main(String args[]) {
-        
-    try {
-        UIManager.setLookAndFeel (new FlatIntelliJLaf()); 
-    } catch (Exception e) {
-        e.printStackTrace();
-    }
+public class Main {
+    public static void main(String args[]) {
+        try {
+            UIManager.setLookAndFeel (new FlatIntelliJLaf()); 
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
 
-    new DashboardUI("administrator").setVisible(true); // shows adminUI (here for testing only, remove in final application)
-    new DashboardUI("officer").setVisible(true); // shows officerUI (here for testing only, remove in final application)
-    new DashboardUI("salesperson").setVisible(true); // shows salespersonUI (here for testing only, remove in final application)
-       
+        new DashboardUI("argumentValue").setVisible(true);
+
+    }
 }
+
 
         
         
