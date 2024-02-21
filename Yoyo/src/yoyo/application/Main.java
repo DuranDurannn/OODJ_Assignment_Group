f38e520b27b643273316a9d190936f001fbe7e68
Xenon0307 <wa9398d@gmail.com>
2024-02-21 16:06:11 +0800

    restructured file handling to include encryption

--- Yoyo/src/yoyo/application/Main.java
+++ Yoyo/src/yoyo/application/Main.java
@@ -2,6 +2,7 @@ package yoyo.application;
 
 import com.formdev.flatlaf.FlatIntelliJLaf;
 import java.io.IOException;
+import java.util.ArrayList;
 import javax.swing.UIManager;
 import javax.swing.UnsupportedLookAndFeelException;
 
@@ -9,14 +10,36 @@ import javax.swing.UnsupportedLookAndFeelException;
 public class Main {
     public static void main(String args[]) throws IOException {
 
-        Login userInfo = new Login();
-        User user = null;
+        String var1 = "darren@gmail.com";
+        String var2 = "123";
+        Login login = new Login();
 
         try {
             UIManager.setLookAndFeel (new FlatIntelliJLaf());
-            new LoginRegisterUI().setVisible(true);
-            user = userInfo.readUserInfo();
-            new DashboardUI(user.getAccessLevel()).setVisible(true); 
+            
+            //new LoginRegisterUI().setVisible(true);
+            
+            
+            FileHandler fileHandler = new FileHandler("userInfo.txt", 7, "Your16CharKey123");
+            ArrayList<String[]> tempDecryptedUserInfoLine = fileHandler.readLinesOneByOne();
+            User verifiedUser = login.processUserInfo(tempDecryptedUserInfoLine, var1, var2);
+            
+            if (verifiedUser != null) {
+                System.out.println("User information:");
+                System.out.println("ID: " + verifiedUser.getID());
+                System.out.println("Username: " + verifiedUser.getUsername());
+                System.out.println("Password: " + verifiedUser.getPassword());
+                System.out.println("Email: " + verifiedUser.getEmail());
+                System.out.println("Address: " + verifiedUser.getAddress());
+                System.out.println("Phone Number: " + verifiedUser.getPhoneNumber());
+                System.out.println("Gender: " + verifiedUser.getGender());
+                System.out.println("Access Level: " + verifiedUser.getAccessLevel());
+            } else {
+                System.out.println("User not found or invalid credentials.");
+            }
+            
+            //new Dashboard(user.getAccessLevel()).setVisible(true); 
+            
         } catch (UnsupportedLookAndFeelException e) {
             //ERROR HANDLING HEREEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEE
         }  
